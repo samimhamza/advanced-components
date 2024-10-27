0f65c3003fc0d8f15c1b22ac9a8afe125e977b71
samimhamza <samim.hamza7@gmail.com>
2024-10-27 16:26:38 +0430

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Livewire\DemoForm;
 use App\Livewire\DemoInfolist;
+use App\Livewire\DemoTable;
 use App\Livewire\TestForm;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', TestForm::class);
-// Route::get('/', DemoForm::class);
-Route::get('/', DemoInfolist::class);
+Route::get('/demo', TestForm::class);
+Route::get('/form', DemoForm::class);
+Route::get('/infolist', DemoInfolist::class);
+Route::get('/table', DemoTable::class);
