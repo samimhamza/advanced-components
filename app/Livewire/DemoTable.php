85e59736f6a168bc41c5f97a0f6fb85df4628603
samimhamza <samim.hamza7@gmail.com>
2024-10-28 13:16:36 +0430

    custom filter completed

--- app/Livewire/DemoTable.php
+++ app/Livewire/DemoTable.php
@@ -4,12 +4,16 @@ namespace App\Livewire;
 
 use App\Models\User;
 use App\Tables\Columns\ColorColumn;
+use App\Tables\Filters\DateRangeFilter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Tables;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class DemoTable extends Component implements HasForms, HasTable
@@ -23,6 +27,11 @@ class DemoTable extends Component implements HasForms, HasTable
             ->columns([
                 Tables\Columns\TextInputColumn::make('name'),
                 ColorColumn::make('color'),
+                Tables\Columns\TextColumn::make('email_verified_at')->since()
+            ])
+            ->filters([
+                DateRangeFilter::make('email_verified_at')
+                    ->maxDate(now()),
             ]);
     }
     public function render()
